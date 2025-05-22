8eb18ea6cfe870da477ac020eae541bcebe4cfe3
Sahil-Ishmam <sahiluddinishmam@gmail.com>
2025-05-22 19:32:59 +0600

    local storage added successfully to save the task

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,23 @@ import TaskForm from "./components/TaskForm";
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  const handleTask = (newTask) => {
+    console.log(newTask);
+
+    // spread it :> and make a new array of tasks with new task
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+
+    // save the task in the local storage
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+
   return (
     <div className="min-h-screen bg-gray-100 py-8 px-8">
       <div className="max-w-3xl mx-auto">
         <AppHeader />
-        <TaskForm />
+        <TaskForm  handleTask={handleTask}/>
       </div>
     </div>
   );
